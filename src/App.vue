d9f0268976c8160f3cb5467c52ced049d3fa5c96
Michel <michel_mihelot@hotmail.com>
2021-07-15 23:33:36 -0300

    CSS Nav and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
   <Footer />
 </template>
 
@@ -11,11 +11,34 @@ import Footer from "./components/Footer.vue";
 export default {
   components: {
     Navbar,
-    Footer
-  }
-}
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Maker Your Burguer",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 30px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,24 @@
 <template lang="">
-    <div>
-        <p>Footer</p>
-    </div>
+    <footer>
+        <p>Maker Your Burguer &copy; 2021</p> 
+    </footer>
 </template>
+
 <script>
 export default {
-    name: "Footer"
-}
+  name: "Footer",
+};
 </script>
-<style lang="">
-    
+
+<style scoped>
+footer {
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
 </style>
